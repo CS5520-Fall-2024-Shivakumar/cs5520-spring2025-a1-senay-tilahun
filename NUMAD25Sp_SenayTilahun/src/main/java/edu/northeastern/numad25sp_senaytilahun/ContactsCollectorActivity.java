0f6a18ef79fb2f5a14c273a8335ebd8d8a992056
Senay Tilahun <senay.argaw@gmail.com>
2025-02-21 19:05:28 -0800

    added the FAB

--- NUMAD25Sp_SenayTilahun/src/main/java/edu/northeastern/numad25sp_senaytilahun/ContactsCollectorActivity.java
+++ NUMAD25Sp_SenayTilahun/src/main/java/edu/northeastern/numad25sp_senaytilahun/ContactsCollectorActivity.java
@@ -1,9 +1,6 @@
 package edu.northeastern.numad25sp_senaytilahun;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,34 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class ContactsCollectorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_contacts_collector);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    // method to display the About me activity
-    public void openAboutMe(View view) {
-        Intent intent = new Intent(this, AboutMeActivity.class);
-        startActivity(intent);
-    }
-
-    // method to handle quic calc button click
-    public void openQuicCalc(View view) {
-        Intent intent = new Intent(this, QuicCalcActivity.class);
-        startActivity(intent);
-    }
-
-    public void openContactsCollector(View view) {
-        Intent intent = new Intent(this, ContactsCollectorActivity.class);
-        startActivity(intent);
     }
 }
