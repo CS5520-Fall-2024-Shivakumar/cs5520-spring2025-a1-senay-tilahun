ea331829120e6b9fc44788b6943c1e7fcb40b1d5
Senay Tilahun <senay.argaw@gmail.com>
2025-03-18 20:06:01 -0700

    Initialized UI elements in FindPrimeActivity

--- NUMAD25Sp_SenayTilahun/src/main/java/edu/northeastern/numad25sp_senaytilahun/FindPrimeActivity.java
+++ NUMAD25Sp_SenayTilahun/src/main/java/edu/northeastern/numad25sp_senaytilahun/FindPrimeActivity.java
@@ -2,6 +2,10 @@ package edu.northeastern.numad25sp_senaytilahun;
 
 import android.os.Bundle;
 
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -10,15 +14,33 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class FindPrimeActivity extends AppCompatActivity {
 
+     TextView textCurrentNumber, textLatestPrime;
+     Button buttonFindPrimes, buttonTerminateSearch;
+     CheckBox checkboxPacifier;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_find_prime);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // init UI elements
+        initUI();
+
+        // TODO: Set onClickListeners in the next step.
+    }
+
+    void initUI() {
+        textCurrentNumber = findViewById(R.id.text_current_number);
+        textLatestPrime = findViewById(R.id.text_latest_prime);
+        buttonFindPrimes = findViewById(R.id.button_find_primes);
+        buttonTerminateSearch = findViewById(R.id.button_terminate_search);
+        checkboxPacifier = findViewById(R.id.checkbox_pacifier);
     }
 }
